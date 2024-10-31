8cc0372c3673a447847e6d6ee32b849120b9afec
Amiel Brencis Salipande <absalipande@gmail.com>
2024-11-01 01:55:05 +0800

    feat: Partially setup the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,49 @@
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { RecentPost } from './components/post';
-import { client } from './lib/client';
+import { Heading } from './components/heading';
+import { Check, Star } from 'lucide-react';
+import { MaxWidthWrapper } from './components/max-width-wrapper';
+import { Button } from './components/button';
 
-export default async function Home() {
-  const res = await client.post.recent.$get();
-  const recentPost = await res.json();
-
-  /**
-   * This is the intended way to prefetch data on the server to have it immediately available in the client.
-   * But: you could also just pass the post as a prop instead of using the `HydrationBoundary`
-   */
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryFn: () => recentPost,
-    queryKey: ['get-recent-post'],
-  });
-
+const Page = () => {
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <main className='flex min-h-screen flex-col items-center justify-center'>
-        <div className='container flex flex-col items-center justify-center gap-12 px-4 py-16'>
-          <h1 className='text-5xl font-medium font-heading tracking-tight sm:text-[5rem]'>
-            <span className='text-brand-700'>j</span>stack
-          </h1>
+    <>
+      <section className='relative py-24 sm:py-32 bg-brand-25'>
+        <MaxWidthWrapper className='text-center'>
+          <div className='relative mx-auto text-center flex flex-col items-center gap-10'>
+            <div>
+              <Heading>
+                <span>Real-Time SaaS Insights,</span>
+                <br />
+                <span className='relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text'>Delivered to Your Discord</span>
+              </Heading>
+            </div>
 
-          <p className='text-center max-w-prose text-balance'>The modern way to confidently ship high-performance, low-cost Next.js apps. End-to-end typesafe with an incredible DX.</p>
+            <p className='text-base/7 text-gray-600 max-w-prose text-center text-pretty'>
+              Mercury makes monitoring your SaaS effortless. Receive real-time alerts for <span className='font-semibold text-gray-700'>sales, new users, or any other event</span> directly in your
+              Discord.
+            </p>
 
-          <RecentPost />
-        </div>
-      </main>
-    </HydrationBoundary>
+            <ul className='space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-start'>
+              {['Instant alerts on Discord for key events', 'One-time purchase, lifetime access', 'Track sales, new users, and custom events in real time'].map((item, index) => (
+                <li key={index} className='flex gap-1.5 items-center text-left'>
+                  <Check className='size-5 shrink-0 text-brand-700' />
+                  {item}
+                </li>
+              ))}
+            </ul>
+
+            <div className='w-full max-w-80'>
+              <Button href='/sign-up' className='relative z-10 h-14 text-base shadow-lg transition-shadow duration-300 hover:shadow-xl'>
+                Sign Up Free and Get Started
+              </Button>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
   );
-}
+};
+
+export default Page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,36 @@
-import type { Config } from "tailwindcss"
-import { fontFamily } from "tailwindcss/defaultTheme"
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
-        heading: ["var(--font-heading)", ...fontFamily.sans],
+        heading: ['var(--font-heading)', ...fontFamily.sans],
       },
       colors: {
         brand: {
-          25: "#F7FAFD",
-          50: "#F0F5FA",
-          100: "#E1EBF5",
-          200: "#C7D7EB",
-          300: "#A9C3DE",
-          400: "#8BAECF",
-          500: "#6D99BF",
-          600: "#5483A3",
-          700: "#426A86",
-          800: "#335269",
-          900: "#273E4F",
-          950: "#1C2D3A",
+          25: '#F7FAFD',
+          50: '#F0F5FA',
+          100: '#E1EBF5',
+          200: '#C7D7EB',
+          300: '#A9C3DE',
+          400: '#8BAECF',
+          500: '#6D99BF',
+          600: '#5483A3',
+          700: '#426A86',
+          800: '#335269',
+          900: '#273E4F',
+          950: '#1C2D3A',
         },
+        'discord-background': '#36393f',
+        'discord-brand-color': '#5865f2',
+        'discord-gray': '#36393f',
+        'discord-text': '#dcddde',
+        'discord-timestamp': '#72767d',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
